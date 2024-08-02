f71fde3051b044bc3c753e224452a913a84036b4
Konstantin Chaika <pro100kot14@gmail.com>
2024-08-02 16:36:47 +0400

    Add placeholders and fix test

--- introduction/page_structure/visual attributes/tests/test_task.py
+++ introduction/page_structure/visual attributes/tests/test_task.py
@@ -13,7 +13,7 @@ class TestCase(unittest.TestCase):
         try:
             self.assertTrue(soup.h1["style"] == "color: blue;", f"Should be color: blue style, got {soup.h1['style']}")
             self.assertTrue("hidden" in str(soup.p), f"Should be hidden paragraph")
-            self.assertTrue("disabled" in str(soup.button), f"Should be disabled button")
+            self.assertTrue("disabled" in str(soup.button).split('>')[0], f"Should be disabled button")
 
         except Exception as e:
             self.fail("wrong page structure")
